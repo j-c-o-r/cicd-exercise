1aa04e23410254284cc23c24d5e3be0141475845
j-c-o-r <40064297+j-c-o-r@users.noreply.github.com>
2022-05-09 11:50:26 +0100

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,12 +1,12 @@
 from locust import HttpUser, task, between
 
 class WebsiteTestUser(HttpUser):
-    wait_time = between(0.5, 3.0)
+    wait_time = between(5, 15)
 
     @task(1)
     def test1(self):
-        self.client.get("http://localhost:5000")
+        self.client.get("http://https://flask-exercise-service.azurewebsites.net")
 
     @task(2)
     def test2(self):
-        self.client.post("http://localhost:5000/predict")
+        self.client.post("https://flask-exercise-service.azurewebsites.net/predict")
